31b2904f6cef270d119612d9025c135a89da6a5b
Tthanh <tdthanh.sgt@gmail.com>
2019-09-18 13:18:21 +0700

    edit order web tthanh

--- src/main/java/web/controller/ApiErrorHandler.java
+++ src/main/java/web/controller/ApiErrorHandler.java
@@ -2,47 +2,20 @@ package web.controller;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import web.exception.ApiException;
-import web.exception.ParameterInvalidException;
 import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @ControllerAdvice
 public class ApiErrorHandler {
 
     private Logger log = Logger.getLogger(ApiErrorHandler.class);
 
-    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
-    public Error handleHttpRequestMethodNotSupportedException(
-            HttpRequestMethodNotSupportedException ex) {
-
-        log.error("Invalid Method", ex);
-
-        return new Error(HttpStatus.METHOD_NOT_ALLOWED.value(),
-                HttpStatus.METHOD_NOT_ALLOWED.getReasonPhrase(),
-                "Api with wrong method");
-    }
-
     @ExceptionHandler(ApiException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleHttpRequestMethodNotSupportedException(
             ApiException ex) {
         return "order";
-    }
-
-    @ExceptionHandler(ParameterInvalidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Error handleHttpBadRequestException(
-            ParameterInvalidException ex) {
-
-        log.error("Invalid Parameter", ex);
-
-        return new Error(HttpStatus.BAD_REQUEST.value(),
-                HttpStatus.BAD_REQUEST.getReasonPhrase(),
-                "Form with Invalid Parameter");
     }
 }
--- src/main/java/web/controller/ErrorPageController.java
+++ src/main/java/web/controller/ErrorPageController.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class ErrorPageController implements ErrorController {
